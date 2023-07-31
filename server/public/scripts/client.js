fbc4aa055b281368a5907945c5e3d07aeeb400ec
Rafael Silva <rafsilva004@gmail.com>
2023-07-31 10:25:42 -0500

    added addNewKoala function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,26 @@
-console.log( 'js' );
+let koalaList = []
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // axios call to server to get koalas
-  
-} // end getKoalas
+  function getKoalas() {
+    console.log( 'in getKoalas' );
+    // axios call to server to get koalas
+    axios.get('/koalas/koalasList').then((response) => {
+      koalaList = response.data;
+      for (let i in koalaList) {
+        viewKoalas.innerHTML += `
+        <tr>
+        <td>${koalaList[i].koalaName}</td>
+        <td>${koalaList[i].sex}</td>
+        <td>${koalaList[i].age}</td>
+        <td>${koalaList[i].readyToTransfer}</td>
+        <td>${koalaList[i].notes}</td>
+        </tr>
+        `;
+      }
+    }).catch((error) => {
+      console.log(error);
+      alert('Something went wrong.');
+  }); 
+    }// end getKoalas
 
 function saveKoala(){
   console.log( 'in saveKoala' );
@@ -12,4 +28,29 @@ function saveKoala(){
  
 }
 
-getKoalas();
+function addNewKoala(event){
+event.preventDefault()
+let name = document.querySelector('#nameIn').value
+let age = document.querySelector('#ageIn').value
+let sex = document.querySelector('#sexIn').value
+let readyToTransfer = document.querySelector('#readyForTransferIn').value
+let notes = document.querySelector('#notesIn').value
+let newKoalaAdded = {
+  name: koalaName,
+  age: age, 
+  sex: sex,
+  readyToTransfer: Transfer,
+  notes: notes, 
+}
+console.log(newKoalaAdded);
+}
+axios.post('/koalas/koalas' , newKoalaAdded).then((response) => {
+  console.log(response);
+  age = ''
+  sex = ''
+  notes = ''
+  getKoalas();
+}).catch((error) => {
+  console.log(error);
+  alert('somethins went wrong')
+});
